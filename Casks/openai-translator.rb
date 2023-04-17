a7e0b114c05fd0fee4fe47b8d28fb8950cc89a68
Letu Wang <wangletu57@gmail.com>
2023-04-16 19:46:26 -0700

    add live check

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -4,14 +4,19 @@ cask "openai-translator" do
     version "0.0.36"
     sha256 arm: "0edfecd0984f36d5828f45c60e985b570862f2d9fefa1851f92799ec6cec630a",
          intel: "55cff09700d3dd4f634142e1fd360b8c2b5289e79f8a1557d06a278d6ad1c6c8"
-  
+
     url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
     name "OpenAI Translator"
     desc "Cross-platform desktop application for translation based on ChatGPT API."
     homepage "https://github.com/yetone/openai-translator"
-  
+
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+
     app "OpenAI Translator.app"
-    
+
     postflight do
         system_command '/usr/bin/xattr',
                 args: [
